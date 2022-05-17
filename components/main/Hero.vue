fdee258cce6f5ed79812d12477145397b9c1594e
hoh1l <hoh1l@unicornpower.space>
2022-05-17 16:00:55 +0300

    Update Hero.vue

--- components/main/Hero.vue
+++ components/main/Hero.vue
@@ -24,7 +24,7 @@
       src="https://telegram.org/js/telegram-widget.js?19"
       :data-telegram-login="BOT_USERNAME"
       data-size="medium"
-      
+      :data-auth-url="REDIRECT_URI"
       :data-onauth="onTelegramAuth(user)"
       data-request-access="write")
   
@@ -68,7 +68,7 @@ export default {
 
   methods: {
     onTelegramAuth(user) {
-      alert(
+      console.log(
         "Logged in as " +
           user.first_name +
           " " +
